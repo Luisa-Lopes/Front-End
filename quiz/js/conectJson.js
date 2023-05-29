e83b99f4dd8deb0a4e7b872262ee82437ad6960b
Luísa Lopes <95827029+Luisa-Lopes@users.noreply.github.com>
2023-05-28 21:18:38 -0300

    Update conectJson.js

--- quiz/js/conectJson.js
+++ quiz/js/conectJson.js
@@ -1,5 +1,5 @@
 async function getJson(){
-    const connection = await fetch("https://luisa-lopes.github.io/Front-End/quiz/data.json");
+    const connection = await fetch("../data.json");
     const convertedConnection = await connection.json();
     return convertedConnection;
 }
